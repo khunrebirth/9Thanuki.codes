f34315f21a4013d6e3c516a6c4f18d25771792d0
khunrebirth <khunrebirth@gmail.com>
2018-11-22 23:38:03 +0700

    update: admin dashboard

--- resources/views/back-end/home.blade.php
+++ resources/views/back-end/home.blade.php
@@ -105,23 +105,6 @@
                 <!-- end: Leave Comment Section -->
 
             </div>
-            {{----}}
-            {{--<div class="col-md-8">--}}
-            {{--<div class="card">--}}
-            {{--<div class="card-header">Dashboard</div>--}}
-
-            {{--<div class="card-body">--}}
-            {{--@if (session('status'))--}}
-            {{--<div class="alert alert-success">--}}
-            {{--{{ session('status') }}--}}
-            {{--</div>--}}
-            {{--@endif--}}
-
-            {{--You are logged in!--}}
-            {{--</div>--}}
-            {{--</div>--}}
-            {{--</div>--}}
-
         </div>
     </div>
 @endsection
